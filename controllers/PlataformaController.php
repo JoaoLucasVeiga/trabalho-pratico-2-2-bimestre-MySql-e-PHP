d2e625a85bcc23657f5a41f355aaaa3218480c56
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:05:14 -0300

    Update PlataformaController.php

--- controllers/PlataformaController.php
+++ controllers/PlataformaController.php
@@ -1,4 +1,19 @@
 <?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Responsável por gerenciar as operações de listar, criar, editar, atualizar e excluir
+ ******************************************************************************/
+
 require_once __DIR__ . '/../models/Plataforma.php';
 
 class PlataformaController {
