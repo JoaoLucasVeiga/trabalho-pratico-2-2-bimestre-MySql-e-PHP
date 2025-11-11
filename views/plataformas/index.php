0e5e03123781f166c9b11852af157f6eab930c37
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:26:48 -0300

    Update index.php

--- views/plataformas/index.php
+++ views/plataformas/index.php
@@ -1,4 +1,22 @@
-<?php include_once __DIR__ . '/../templates/header.php'; ?>
+<?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Esse código exibe a view de listagem e gerenciamento das plataformas, 
+ mostrando uma tabela com todas as plataformas cadastradas e botões para editar,
+ excluir ou adicionar uma nova, com um aviso de confirmação para evitar exclusões acidentais.
+ ******************************************************************************/
+
+include_once __DIR__ . '/../templates/header.php'; ?>
 
 <div class="page-header">
     <h2>Gerenciamento de Plataformas</h2>
