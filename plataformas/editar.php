7632ce82b2347c38632f5b6c7db82424abf8d1a6
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:15:55 -0300

    Add header documentation to editar.php
    Added header comments with course and project details.

--- plataformas/editar.php
+++ plataformas/editar.php
@@ -1,4 +1,19 @@
-<?php include_once __DIR__ . '/../templates/header.php'; ?>
+<?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Esse código exibe a view de edição de uma plataforma, mostrando um formulário preenchido com os dados atuais da plataforma selecionada, permitindo que o usuário altere o nome e envie as mudanças para o controlador.
+ ******************************************************************************/
+include_once __DIR__ . '/../templates/header.php'; ?>
 
 <h2>Editar Plataforma</h2>
 
