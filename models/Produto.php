dd35358334e44a769fceb32209cc31baee3c703f
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-10 21:56:28 -0300

    Add header comments to Produto.php
    Added header comments with course and team details.

--- models/Produto.php
+++ models/Produto.php
@@ -1,4 +1,19 @@
 <?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Gerencia as operações do banco de dados relacionadas aos produtos.
+ ******************************************************************************/
+
 class Produto {
     private $pdo;
     public function __construct($pdo) { $this->pdo = $pdo; }
