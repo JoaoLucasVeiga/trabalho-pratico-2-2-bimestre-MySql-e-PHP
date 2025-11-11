739229a97c9e5507fbe17a7e08c6ab3d905e5b81
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:28:33 -0300

    Add header comments to editar.php
    Added header comments detailing course information and purpose of the edit product view.

--- views/produtos/editar.php
+++ views/produtos/editar.php
@@ -1,4 +1,22 @@
-<?php include_once __DIR__ . '/../templates/header.php'; ?>
+<?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Esse código exibe a view para editar um produto existente, apresentando um 
+ formulário pré-preenchido com as informações atuais do produto, incluindo imagem de capa, 
+ nome, descrição, preço e plataforma, permitindo também enviar uma nova imagem ou manter a antiga ao atualizar os dados.
+ ******************************************************************************/
+
+include_once __DIR__ . '/../templates/header.php'; ?>
 
 <h2>Editar Produto</h2>
 
