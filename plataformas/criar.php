c1aa7cd7d3deb536f645b78c5caad3cdf061712c
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:12:46 -0300

    Add course information comments to criar.php
    Added header comments for course details and purpose.

--- plataformas/criar.php
+++ plataformas/criar.php
@@ -1,4 +1,20 @@
-<?php include_once __DIR__ . '/../templates/header.php'; ?>
+<?php 
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Para que o usuário insira o nome da plataforma e envie os dados ao controlador
+ ******************************************************************************/
+
+include_once __DIR__ . '/../templates/header.php'; ?>
 
 <h2>Adicionar Nova Plataforma</h2>
 
