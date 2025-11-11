3001746b1e1079758e6d46ef75e2e16673cb04a8
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:29:54 -0300

    Add course and project details to index.php
    Added header comment with course and project details.

--- views/produtos/index.php
+++ views/produtos/index.php
@@ -1,4 +1,24 @@
-<?php include_once __DIR__ . '/../templates/header.php'; ?>
+<?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Esse código mostra a página principal de produtos. Ele lista todos os 
+ jogos cadastrados, com imagem, nome, preço e plataforma. Também tem uma barra de 
+ busca para filtrar os produtos e botões para editar ou excluir cada item. Quando não há 
+ produtos cadastrados ou o termo pesquisado não é encontrado, aparece uma mensagem informando 
+ isso. No final, há a paginação para navegar entre várias páginas de resultados.
+ ******************************************************************************/
+
+include_once __DIR__ . '/../templates/header.php'; ?>
 
 <div class="page-header">
     <div>
