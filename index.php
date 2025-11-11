4c3a4e882486e4e58198dc570cc5e200df1eaa2c
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-11 20:36:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+/*******************************************************************************
+ * Curso: Engenharia de Software
+ * Disciplina: Linguagem e Técnicas de Programação
+ * Professor: Flores
+ * Turma: ESOFT-2B
+ * Componentes:
+ *     25291090-2 - João Lucas Veiga de Carvalho
+ *     25164719-2 - Rafael Maia
+ *     25033056-2 - Gabriel Michels Cubas
+ *     25001118-2 - Emanuel Gomes de Almeida
+ *     25361613-2 - Arthur Marinho Figueira
+ *     25362639-2 - Roberti Mendes de moura
+ * Descritivo: Esse código é o roteador principal do sistema. Ele inicia a sessão, conecta ao 
+ banco de dados, carrega os controladores e executa a ação informada na URL (como listar, criar, 
+ editar ou excluir produtos e plataformas). Se nenhuma ação for passada, ele mostra a lista de produtos por padrão.
+ ******************************************************************************/
+
+
 session_start();
 
 require_once __DIR__ . '/config/database.php';
