3ea3a763583d003c7442686563b1ac8ecc59b7cf
Emanuel Almeida <emanuelgomesalmeida@gmail.com>
2025-11-10 22:00:49 -0300

    Update criar.php

--- views/plataformas/criar.php
+++ views/plataformas/criar.php
@@ -1,20 +1,4 @@
-<?php
-/*******************************************************************************
- * Curso: Engenharia de Software
- * Disciplina: Linguagem e Técnicas de Programação
- * Professor: Flores
- * Turma: ESOFT-2B
- * Componentes:
- *     25291090-2 - João Lucas Veiga de Carvalho
- *     25164719-2 - Rafael Maia
- *     25033056-2 - Gabriel Michels Cubas
- *     25001118-2 - Emanuel Gomes de Almeida
- *     25361613-2 - Arthur Marinho Figueira
- *     25362639-2 - Roberti Mendes de moura
- * Descritivo: Exibe o formulário para adicionar um novo produto
- ******************************************************************************/
-
-include_once __DIR__ . '/../templates/header.php'; ?>
+<?php include_once __DIR__ . '/../templates/header.php'; ?>
 <h2>Adicionar Novo Produto</h2>
 <form action="index.php?action=salvar_produto" method="POST">
     <label for="nome">Nome do Jogo:</label>
